eff59203d4d253630b34b36cc72c060f371637af
Halfmoon0707 <moryan0001@gmail.com>
2024-04-24 15:10:16 +0530

    Parallel Pharmacy

---
+++ frontend/firebase.py
@@ -0,0 +1,9 @@
+# import firebase_admin
+# from firebase_admin import credentials, db , storage
+
+# cred = credentials.Certificate(r"scanPlus-main1\scanPlus-main\frontend\parellel-pahrmacy-firebase-adminsdk-aglf1-930030001e.json")  # Path to your Firebase service account JSON file
+# firebase_admin.initialize_app(cred, {
+#     'databaseURL': 'https://parellel-pahrmacy-default-rtdb.firebaseio.com/'
+# })
+
+# firebase_ref = db.reference('/users')  
